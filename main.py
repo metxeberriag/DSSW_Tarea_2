d4fda58e438d548473d10a489d743389e7dbc051
metxeberriag <mikel@MacBook-Pro-de-MIKEL.local>
2016-12-12 11:52:50 +0100

    Version 3

--- main.py
+++ main.py
@@ -8,28 +8,52 @@ class RegisterHandler(webapp2.RequestHandler):
         self.response.write('''
         <html>
             <head>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
                 <script>
-                $(document)
-                    .on('click','form input[type=submit]', function(e) {
-                        if ($('#password1').val()==''){
-                            $('#error').text('Las contraseñas no son las mismas!');
-                            e.preventDefault();
-                        }
-                    });
+                $(document).on('click', '#enviar', function(e) {
+                    $('#errorEmail').text(' ');
+                    $('#errorPassword').text(' ');
+                    
+                    if ($('#password1').val()!=$('#password2').val()) {
+                        $('#errorPassword').text('Las contraseñas no son las mismas!');
+                        e.preventDefault();
+                    }
+                    
+                    if (!validateEmail($('#email').val())) {
+                        $('#errorEmail').text('El email está mal escrito o está sin rellenar!');
+                        e.preventDefault();
+                    }
+                });
+                
+                function validateEmail(email) {
+                    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                    return regex.test(email);
+                }
                 </script>
             </head>
             <body>
-                <form method='POST' id='tarea2'>
-                    <label>Nombre de usuario:</label>
-                    <input type='text' name='nombre' id='nombre' placeholder='Nombre' required='true'> <br>
-                    <label>Password:</label>
-                    <input type='password' name='password1' id='password1' placeholder='Password' required='true'> <br>
-                    <label>Repetir Password:</label>
-                    <input type='password' name='password2' id='password2' placeholder='Repetir el mismo de antes' required='true'> <br>
-                    <label>Email:</label>
-                    <input type='text" name='email' id='email' placeholder='Email' required='true'> <br>
-                    <br><input type='submit'>
-                    <span id='error'></span>
+                <form method="post" id="login">
+                    <table>
+                        <tr>
+                            <td class="label"> Nombre de usuario </td>
+                            <td> <input type="text" name="username" id="username" value="" placeholder="Tu nombre..." required="true"> </td>
+                        </tr>
+                        <tr>
+                            <td class="label"> Password </td>
+                            <td> <input type="password" name="password" id="password1" value="" placeholder="Tu contraseña..."required="true"></td>
+                        </tr>
+                        <tr>
+                            <td class="label">Repetir Password </td>
+                            <td> <input type="password" name="password2" id="password2" value="" placeholder="El mismo de antes" required="true"> </td>
+                            <td> <span id="errorPassword" style="color:red"></span> </td>
+                        </tr>
+                        <tr>
+                            <td class="label"> Email </td>
+                            <td> <input type="text" name="email" value="" id="email" placeholder="Tu email..." required="true"> </td>
+                            <td> <span id="errorEmail" style="color:red"></span> </td>
+                        </tr>
+                    </table>
+                    <input type="submit" id="enviar">
                 </form>
             </body>
         </html>''')
@@ -39,8 +63,8 @@ class RegisterHandler(webapp2.RequestHandler):
             <html>
                 <body>
                     <h1>Formulario recibido correctamente</h1>'''
-                    + "Hola " + self.request.get('nombre')+'''<br>'''
-                    + "Tu email es " + self.request.get('email')+'''<br>
+                    + "Hola " + self.request.get('username')+'''<br>'''
+                    + "Tu email es " + self.request.get('email')+'''<br><br>
                     <a href="/registro">Volver</a>
                 </body>
             </html>''')
